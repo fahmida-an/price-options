f81cb97db87bc605a3bca7f1ac2a1109a7575c49
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-09-17 22:53:21 +0600

    Added simple navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+// import Navdaisy from './Components/Navdaisy/Navdaisy'
+
 
 function App() {
 
 
   return (
     <>
-     
-      
+     {/* <Navdaisy></Navdaisy> */}
+      <Navbar></Navbar>
    
     </>
   )
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import Link from "../Link/Link";
+const Navbar = () => {
+    const routes = [
+        { id: 1, path: '/', name: 'Home' },
+        { id: 2, path: '/about', name: 'About' },
+        { id: 3, path: '/contact', name: 'Contact' },
+        { id: 4, path: '/products', name: 'Products' },
+        { id: 5, path: '/product/:id', name: 'Product Detail' },
+      ];
+      
+      
+      
+    return (
+        <nav>
+            <ul className="md:flex">
+            {
+                routes.map(route => <Link key ={route.id} route ={route}></Link> )
+            }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
